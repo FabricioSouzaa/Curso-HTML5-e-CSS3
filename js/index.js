47f5c2dfd4a87cbf53abd03795506c55eac88daf
Fabrício <souza.fabricio8@gmail.com>
2022-11-16 07:51:28 -0300

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-console.log('Fabrício');  // String
-console.log("Fabrício");
-console.log(`Fabrício`);
-console.log(123456, 15.35, 'Fabrício Souza');
-
-console.log('Meu nome é "Fabrício". Estou aprendendo JavaScript às', 06, 'da manhã');  // Deste modo posso adicionar um comentário.
