8e95d9ddeb6f13be1baea7a2ae7990a4d61621d0
Fabrício <souza.fabricio8@gmail.com>
2023-10-11 07:51:56 -0300

    Regras para declarar variáveis

--- Aulas/Aula06/index.js
+++ Aulas/Aula06/index.js
@@ -11,3 +11,12 @@ console.log(nome, 'nasceu em 2001');
 console.log('Em 2020', nome, 'conheceu Bárbara');
 console.log(nome, 'casou-se com Barbara em 2022.');
 console.log('Barbara teve 1 filho com', nome, 'em 2023.');
+
+/* 'Regras' para criar variáveis:
+    * Não podemos utilizar palavras reservadas.
+    * Precisam ter nomes significativos/objetivos.
+    * Não pode começar com um número.
+    * Não pode conter 'espaços' ou 'traços'. 
+    * Utilizamos camelCase: Por exemplo: nomeCliente
+    * Case-sensitive
+*/
