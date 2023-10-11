d131e5a8bdfd2baa503e72cae4589aec56ce0e7c
Fabrício <souza.fabricio8@gmail.com>
2023-10-11 07:28:05 -0300

    Aula06 - Exercícios

---
+++ Aulas/Aula06/index.js
@@ -0,0 +1,6 @@
+let nome = 'Fabrício';
+
+console.log(nome, 'nasceu em 2001');
+console.log('Em 2020', nome, 'conheceu Bárbara');
+console.log(nome, 'casou-se com Barbara em 2022.');
+console.log('Barbara teve 1 filho com', nome, 'em 2023.');
