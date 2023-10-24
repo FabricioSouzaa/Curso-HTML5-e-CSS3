579e97238e762d907b580cdd82132a73cf99b8ee
Fabrício <souza.fabricio8@gmail.com>
2023-10-24 07:04:31 -0300

    Exercises

--- Aulas/Aula07/index.js
+++ Aulas/Aula07/index.js
@@ -1,5 +1,6 @@
 const primeiroNumero = 5;
 const segundoNumero = 10;
-const resultado = primeiroNumero + segundoNumero;
+const resultado = primeiroNumero * segundoNumero;
 const resultadoDuplicado = resultado * 2;
 console.log(resultado);
+console.log(resultadoDuplicado)
